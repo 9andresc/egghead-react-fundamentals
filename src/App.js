42df798425ae8eef6cee604f48a38cd2db53a89a
Gustavo <andrescabral.c@gmail.com>
2017-02-01 10:35:48 -0300

    Add custom propType validation to React components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return <Button>I <Heart /> React</Button>;
+    return <Title text="The Text" />;
   }
 }
 
-const Button = props => <button>{props.children}</button>;
+const Title = props => <h1>Title: {props.text}</h1>;
 
-class Heart extends React.Component {
-  render() {
-    return <span>&hearts;</span>;
-  }
-}
+Title.propTypes = {
+  text(props, propName, _component) {
+    if (!(propName in props)) {
+      return new Error(`Missing ${propName}`);
+    }
+
+    if (props[propName].length < 6) {
+      return new Error(`${propName} was too short`);
+    }
+  },
+};
 
 export default App;
