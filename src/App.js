e91926a62faf448c14e1eb6513374e5254364fb7
Gustavo <andrescabral.c@gmail.com>
2017-02-01 10:02:02 -0300

    Set properties on React components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>;
+class App extends React.Component {
+  render() {
+    const txt = this.props.txt;
+    return <h1>{txt}</h1>;
+  }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired,
+};
+
+App.defaultProps = {
+  txt: 'this is the default txt',
+};
 
 export default App;
